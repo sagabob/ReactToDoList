04c484698cd32cb4d89c868214dc780c6c9a4e3c
bobpham.tdp@gmail.com <bobpham.tdp@live.com>
2020-06-13 15:49:35 +1000

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import dependencies
+import * as React from "react";
+import "./styles/styles.css";
+// Import components
+import TodoForm from "./components/todo-form";
+import TodoList from "./components/todo-list";
 
-function App() {
+// Import interfaces
+import { TodoInterface } from "./interfaces";
+
+// Import styles
+import "./styles/styles.css";
+
+// TodoListApp component
+const App = () => {
+  const [todos, setTodos] = React.useState<TodoInterface[]>([]);
+
+  // Creating new todo item
+  function handleTodoCreate(todo: TodoInterface) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = [...todos];
+
+    // Update new todos state
+    newTodosState.push(todo);
+
+    // Update todos state
+    setTodos(newTodosState);
+  }
+
+  // Update existing todo item
+  function handleTodoUpdate(
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: string
+  ) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = [...todos];
+
+    // Find correct todo item to update
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.text =
+      event.target.value;
+
+    // Update todos state
+    setTodos(newTodosState);
+  }
+
+  // Remove existing todo item
+  function handleTodoRemove(id: string) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = todos.filter(
+      (todo: TodoInterface) => todo.id !== id
+    );
+
+    // Update todos state
+    setTodos(newTodosState);
+  }
+
+  // Check existing todo item as completed
+  function handleTodoComplete(id: string) {
+    // Copy current todos state
+    const newTodosState: TodoInterface[] = [...todos];
+
+    // Find the correct todo item and update its 'isCompleted' key
+    newTodosState.find(
+      (todo: TodoInterface) => todo.id === id
+    )!.isCompleted = !newTodosState.find(
+      (todo: TodoInterface) => todo.id === id
+    )!.isCompleted;
+
+    // Update todos state
+    setTodos(newTodosState);
+  }
+
+  // Check if todo item has title
+  function handleTodoBlur(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.value.length === 0) {
+      event.target.classList.add("todo-input-error");
+    } else {
+      event.target.classList.remove("todo-input-error");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list-app">
+      <TodoForm todos={todos} handleTodoCreate={handleTodoCreate} />
+
+      <TodoList
+        todos={todos}
+        handleTodoUpdate={handleTodoUpdate}
+        handleTodoRemove={handleTodoRemove}
+        handleTodoComplete={handleTodoComplete}
+        handleTodoBlur={handleTodoBlur}
+      />
     </div>
   );
-}
+};
 
 export default App;
